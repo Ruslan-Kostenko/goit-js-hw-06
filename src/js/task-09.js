5623e80b4273a1e7b2f3414180e5f6934687fa34
Ruslan-Kostenko <123192295+Ruslan-Kostenko@users.noreply.github.com>
2023-07-08 20:34:32 +0300

    Update js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -2,7 +2,7 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
 
 const bodyEl = document.querySelector('body');
 const spanEl = document.querySelector('.color');
@@ -14,5 +14,5 @@ function clickOnButton (event) {
   console.log(getRandomHexColor);
   spanEl.textContent = getRandomHexColor();
   bodyEl.style.backgroundColor = getRandomHexColor();
-}
+};
 
